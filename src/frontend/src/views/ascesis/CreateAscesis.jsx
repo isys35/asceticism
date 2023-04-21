03b77bc495601250f5f10bcadf19c2389471c4a2
isys35 <isys35@mail.ru>
2023-04-22 00:32:37 +0300

    fix: add api and field errors

--- src/frontend/src/views/ascesis/CreateAscesis.jsx
+++ src/frontend/src/views/ascesis/CreateAscesis.jsx
@@ -2,7 +2,16 @@ import { InputText } from "primereact/inputtext";
 import { Card } from "primereact/card";
 import { Calendar } from "primereact/calendar";
 import { Button } from "primereact/button";
-import {useFormik} from "formik";
+import { useFormik } from "formik";
+import { classNames } from "primereact/utils";
+import * as Yup from "yup";
+ 
+const CreateAscesisSchema = Yup.object().shape({
+  name: Yup.string()
+    .required("Обязательное поле"),
+  start_date: Yup.date()
+    .required("Обязательное поле"),
+});
 
 function CreateAscesis() {
   const formik = useFormik({
@@ -10,28 +19,41 @@ function CreateAscesis() {
       name: "",
       start_date: ""
     },
+    validationSchema: CreateAscesisSchema,
     onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      console.log(values);
+      // alert(JSON.stringify(values, null, 2));
     },
   });
+
+  const isFormFieldInvalid = (name) => !!(formik.touched[name] && formik.errors[name]);
+
+  const getFormErrorMessage = (name) => {
+    return isFormFieldInvalid(name) ? <small className="p-error">{formik.errors[name]}</small> : <small className="p-error">&nbsp;</small>;
+  };
+
   return (
     <Card>
       <h2>Создание Аскезы</h2>
       <form onSubmit={formik.handleSubmit}>
         <div className="field">
           <label htmlFor="name">Аскеза</label>
-          <InputText
+          <InputText 
+            className={"w-full " + classNames({ "p-invalid": isFormFieldInvalid("name") })}
             id="name"
             value={formik.values.name}
             onChange={formik.handleChange}/>
         </div>
+        {getFormErrorMessage("name")}
         <div className="field">
           <label htmlFor="start_date">Дата начала</label>
           <Calendar
+            className={"w-full " + classNames({ "p-invalid": isFormFieldInvalid("start_date") })}
             id="start_date"
             dateFormat="dd.mm.yy"
             value={formik.values.start_date}
             onChange={formik.handleChange}/>
+          {getFormErrorMessage("start_date")}
         </div>
         <Button label="Создать" type="submit"/>
       </form>
