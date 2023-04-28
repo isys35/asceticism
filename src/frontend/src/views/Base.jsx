39b7bb0830e5c1daf7b3fa6af8bd60a278dee3d9
isys35 <isys35@mail.ru>
2023-04-28 21:47:16 +0300

    fix: verstka

--- src/frontend/src/views/Base.jsx
+++ src/frontend/src/views/Base.jsx
@@ -2,8 +2,10 @@ import { ConfirmPopup, confirmPopup } from "primereact/confirmpopup";
 import { logout } from "../auth/auth.js";
 import { Menubar } from "primereact/menubar";
 import { Outlet } from "react-router-dom";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Toast } from "primereact/toast";
+import { Avatar } from "primereact/avatar";
+import { Sidebar } from "primereact/sidebar";
 
 function Base() {
   const toast = useRef(null);
@@ -23,13 +25,35 @@ function Base() {
       }
     }
   ];
+  const [profileMenu, showProfileMenu] = useState(false);
+
   return (
-    <>
+    <div className="content-wrapper">
+      <div className="topbar">
+        <div className="topbar-start">
+        </div>
+        <div className="topbar-end">
+          <ul className="topbar-menu">
+            <li className="topbar-profile">
+              <button className="p-link" onClick={() => showProfileMenu(true)}>
+                <Avatar icon="pi pi-user" size="large" shape="circle" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Menubar model={menu} />
       <Toast ref={toast} />
       <ConfirmPopup />
       <Outlet context={toast} />
-    </>
+      <Sidebar visible={profileMenu} position="right" onHide={() => showProfileMenu(false)}>
+        <h4>Right Sidebar</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </Sidebar>
+    </div>
   );
 }
 
