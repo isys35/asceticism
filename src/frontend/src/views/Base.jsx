381bacc98b215d6245bb429fd34fc3a193b91379
isys35 <isys35@mail.ru>
2023-05-03 21:42:55 +0300

    fix: sidebar style

--- src/frontend/src/views/Base.jsx
+++ src/frontend/src/views/Base.jsx
@@ -10,6 +10,7 @@ import { BreadCrumb } from "primereact/breadcrumb";
 import { useOutletContext } from "react-router";
 import { breadCrumbsLink } from "../hooks/useBreadcrumbs.jsx";
 import { Button } from "primereact/button";
+import classNames from "classnames";
 
 function Base() {
   const toast = useRef(null);
@@ -25,46 +26,69 @@ function Base() {
   };
   const [breadcrumbs, setBreadcrumbs] = useState([]);
   const [profileMenu, showProfileMenu] = useState(false);
+  const [mainMenu, showMainMenu] = useState(false);
   const email = localStorage.getItem("email");
   const firstName = JSON.parse(localStorage.getItem("first_name"));
   const lastName = JSON.parse(localStorage.getItem("last_name"));
   const home = { icon: "pi pi-home", url: "/", template: breadCrumbsLink };
-
+  
+  const staticClasses = classNames("container", "static", {
+    "static-inactive": !mainMenu
+  });
+  
   return (
-    <div className="content-wrapper">
-      <div className="topbar">
-        <div className="topbar-start">
-          <Button icon="pi pi-bars" className="topbar-menubutton" text rounded severity="secondary"/>
-          <div className="breadcrumbs">
-            {breadcrumbs.length>0 && <BreadCrumb model={breadcrumbs} home={home} />}
+    <div className={staticClasses}>
+      <div className="content-wrapper" >
+        <div className="topbar">
+          <div className="topbar-start">
+            <Button 
+              icon="pi pi-bars" 
+              className="topbar-menubutton" 
+              text rounded severity="secondary" 
+              onClick={() => showMainMenu(!mainMenu)}
+            />
+            <div className="breadcrumbs">
+              {breadcrumbs.length>0 && <BreadCrumb model={breadcrumbs} home={home} />}
+            </div>
+          </div>
+          <div className="topbar-end">
+            <ul className="topbar-menu">
+              <li className="topbar-profile">
+                <button className="p-link" onClick={() => showProfileMenu(true)}>
+                  <Avatar icon="pi pi-user" size="large" shape="circle" />
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="topbar-end">
-          <ul className="topbar-menu">
-            <li className="topbar-profile">
-              <button className="p-link" onClick={() => showProfileMenu(true)}>
-                <Avatar icon="pi pi-user" size="large" shape="circle" />
-              </button>
-            </li>
+        <Toast ref={toast} />
+        <ConfirmDialog />
+        <Sidebar visible={profileMenu} position="right" onHide={() => showProfileMenu(false)}>
+          <div className="flex flex-column mx-auto md:mx-0"><span className="mb-2 font-semibold">{email}</span><span
+            className="text-color-secondary font-medium mb-5">{firstName} {lastName}</span>
+          <ul className="list-none m-0 p-0">
+            <ProfileMenuButton
+              icon="pi-power-off" 
+              mainText="Выход"
+              secondaryText="Выйти из профиля"
+              onClick={confirmLogout}
+            />
           </ul>
-        </div>
+          </div>
+        </Sidebar>
+        <Sidebar
+          appendTo="self"
+          visible={mainMenu}
+          position="left"
+          onHide={() => showMainMenu(false)}
+          modal={false}
+          dismissable={false}
+          showCloseIcon={false}
+        >
+          <p>LOGO</p>
+        </Sidebar>
+        <Outlet context={{toast, breadcrumbs, setBreadcrumbs}} />
       </div>
-      <Toast ref={toast} />
-      <ConfirmDialog />
-      <Sidebar visible={profileMenu} position="right" onHide={() => showProfileMenu(false)}>
-        <div className="flex flex-column mx-auto md:mx-0"><span className="mb-2 font-semibold">{email}</span><span
-          className="text-color-secondary font-medium mb-5">{firstName} {lastName}</span>
-        <ul className="list-none m-0 p-0">
-          <ProfileMenuButton
-            icon="pi-power-off" 
-            mainText="Выход"
-            secondaryText="Выйти из профиля"
-            onClick={confirmLogout}
-          />
-        </ul>
-        </div>
-      </Sidebar>
-      <Outlet context={{toast, breadcrumbs, setBreadcrumbs}} />
     </div>
   );
 }
